b927a12dfc5aa6bbb4c032ef6ed2428347ac111c
Admin <arnabchoudhury2005@gmail.com>
2024-10-15 19:44:39 +0530

    first commit 2 -- fun game.

--- src/PracGame/Game.java
+++ src/PracGame/Game.java
@@ -3,21 +3,14 @@ import java.util.Random;
 import java.util.Scanner;
 public class Game {
 
-    //private static int noOfGuesses = 0;
     /*
     Game default constructor
      */
     // Note: it is not necessary to use constructor we can use method as well
     // Note: Constructor name (Game) is equal to class name (Game) which is created
-//    public int Game() {
-//        Random random = new Random();
-//        int guessNum = random.nextInt(0,10);
-//        //System.out.println("Guess is " + guessNum);
-//        return guessNum;
-//    }
-    public static int Randominput() {
+    public int Game() {
         Random random = new Random();
-        int guessNum = random.nextInt(0, 100);
+        int guessNum = random.nextInt(0,10);
         //System.out.println("Guess is " + guessNum);
         return guessNum;
     }
@@ -31,10 +24,10 @@ public class Game {
         if (n == userIn) {
             return true;
         } else if (n < userIn) {
-            System.out.println("yr value is high !");
+            System.out.println("your value is high !");
             return false;
         } else if (n > userIn) {
-            System.out.println("yr value is low !");
+            System.out.println("Your value is low !");
             return false;
         } else
             return false;
@@ -43,12 +36,11 @@ public class Game {
     public static void main(String[] args) {
         int noOfGuesses = 0;
         boolean ch;
-//        Game player = new Game();
-        int guessNum = Randominput();
+        Game player = new Game();
         do {
             System.out.print("Enter Yr Num :  ");
             int n = takeUserInput();
-            ch = isCorrectNumber(guessNum, n);
+            ch = isCorrectNumber(player.Game(), n);
             noOfGuesses++;
         } while (ch != true);
 
